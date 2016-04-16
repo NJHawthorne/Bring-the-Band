79ea7cdbd453ece9d4c97e2f72876b7864878e50
Nate Hawthorne <natejhawthorne@gmail.com>
2016-04-16 17:20:24 -0500

    Trying to integrate state...

--- app/scripts/components/subcomponents/SearchContent.js
+++ app/scripts/components/subcomponents/SearchContent.js
@@ -4,15 +4,36 @@ import $ from 'jquery';
 // "https://api.spotify.com/v1/search?q={this.refs.search.value}*&type=artist"
 
 const SearchContent = React.createClass({
+	getInitialState: function() {
+		return {
+			results: []
+		}	
+	},
 	handleSearch: function(e) {
 		e.preventDefault();
 		let search = this.refs.search.value;
-		$.get("https://api.spotify.com/v1/search?q="+search+"*&type=artist", function(data) {
-			console.log(data);
+		let newResults = [];
+		$.get("https://api.spotify.com/v1/search?q="+search+"*&type=artist", function(bandData) {
+			newResults = bandData.artists.items.map(function(artists, i, arr) {
+				console.log(artists.name);
+				if (artists.images == false){
+					console.log("No image URL detected");
+				} else {
+					console.log(artists.images[0].url);
+				}
+				return {
+					name: artists.name,
+					url: artists.images
+				};
+			});
 		});
-		console.log(this.refs.search.value);
+		this.setState({results: newResults})
+		console.log(this.state);
+		console.log(this.state.results);
+		// console.log(this.refs.search.value);
 	},
 	render: function() {
+		const spotifyResults = this.state.bandData;
 		return (
 			<section>
 				<form
@@ -23,7 +44,8 @@ const SearchContent = React.createClass({
 						ref="search" />
 					<input 
 						type="submit" />
-				</form>		
+				</form>
+				<header>Showing some results</header>		
 			</section>
 		)
 	}
