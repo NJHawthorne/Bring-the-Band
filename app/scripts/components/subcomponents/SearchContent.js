2ab73e39a58c5e340f4fb61a6881a2e4569a1f28
Nate Hawthorne <natejhawthorne@gmail.com>
2016-06-02 11:58:03 -0500

    Added ability to upvote an existing band as well as upvote a new band

--- app/scripts/components/subcomponents/SearchContent.js
+++ app/scripts/components/subcomponents/SearchContent.js
@@ -13,18 +13,22 @@ const SearchContent = React.createClass({
 		}	
 	},
 	componentDidMount: function() {
+		//fetching the models that already have votes
 		this.state.results.on('update', () => {
 			this.setState({results: this.state.results});
 		});
 		this.state.results.fetch();
 	},
 	render: function() {
+		//storing the search results from Spotify
 		const totalListings = [];
 		totalListings.push(this.state.searchResults);
 		const eachListing = (totalListings[0]).map((val, i, arr) => {
+			//giving the search results an "image" in case they didn't have one
 			if(val.images == false) {
 				val.images.push({url: "http://vignette3.wikia.nocookie.net/max-steel-reboot/images/7/72/No_Image_Available.gif/revision/latest?cb=20130902173013"});
 			}
+			//passing all the search data as props into the subcomponent
 			return (
 				<BandListing 
 					key={val.id}
@@ -51,6 +55,7 @@ const SearchContent = React.createClass({
 	},
 	handleSearch: function(e) {
 		e.preventDefault();
+		//creating the ajax request to Spotify's api so results can be displayed
 		let search = this.refs.search.value;
 		this.searchRequest = $.get("https://api.spotify.com/v1/search?q="+search+"*&type=artist", function(bandData) {
 			let bandList = bandData.artists;
@@ -60,16 +65,29 @@ const SearchContent = React.createClass({
 		}.bind(this));
 	},
 	handleVote: function(newVote) {
+		//making a variable to save some time later
 		let upvotedArtists = this.state.results.models;
+		//making a flag variable in case, in their search, they upvote an entry that already has votes
+		let existingVote = false;
 		for (var i = 0; i < upvotedArtists.length; i++) {
+			//checking to see if the artist they're voting for has already been voted for
 			if(upvotedArtists[i].get('artist') === newVote.artist) {
-				console.log('There was a match');
-				let updatedBand = this.state.results.get(upvotedArtists[i].get('id'));
-				console.log(updatedBand);
-				// updatedBand.save({
-				// 	votes: (upvotedArtists[i].get('votes') + 1)
-				// })
+				let updatedBand = this.state.results.get(upvotedArtists[i].id);
+				//updating the model with one additional vote
+				updatedBand.save({
+					votes: updatedBand.get('votes') + 1
+				});
+				//setting the flag to true so a new model isn't created
+				existingVote = true;
 			}
+		}
+		//checking to see if a new model needs to be made
+		if(!existingVote) {
+			this.state.results.create({
+				artist: newVote.artist,
+				thumbnail: newVote.thumbnail,
+				votes: 1
+			});
 		}
 	}
 });
